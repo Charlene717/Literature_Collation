c7533721f427e163c0a230be4d538cac5962bb56
Charlene717 <q2330416@gmail.com>
2021-04-12 10:48:37 +0800

    Add csv files

--- Web_of_Science_JCR.R
+++ Web_of_Science_JCR.R
@@ -134,16 +134,20 @@ NewTable4 <- NewTable4[!is.na(NewTable4$Publication.Year),]
 NewTable5_R1_Y1 <- NewTable4[NewTable4$Journal.Impact.Factor > 3 & NewTable4$Journal.Impact.Factor <= 5 & NewTable4$Publication.Year ==2021,]
 write.table(NewTable5_R1_Y1,file=paste0(PathName,"/",RVersion,"/",RVersion,"_Candidate_AuthorV3_TIFR1_Y1.txt"),
             row.names = F,col.names = TRUE, sep = '\t')
+write.table(NewTable5_R1_Y1,file=paste0(PathName,"/",RVersion,"/",RVersion,"_Candidate_AuthorV3_TIFR1_Y1.csv"),row.names = FALSE,sep = ',')
 
   #R2
 NewTable5_R2_Y1 <- NewTable4[NewTable4$Journal.Impact.Factor > 5 & NewTable4$Journal.Impact.Factor <= 7 & NewTable4$Publication.Year ==2021,]
 write.table(NewTable5_R2_Y1,file=paste0(PathName,"/",RVersion,"/",RVersion,"_Candidate_AuthorV3_TIFR2_Y1.txt"),
             row.names = F,col.names = TRUE, sep = '\t')
+write.table(NewTable5_R2_Y1,file=paste0(PathName,"/",RVersion,"/",RVersion,"_Candidate_AuthorV3_TIFR2_Y1.csv"),row.names = FALSE,sep = ',')
+
 
 #R3
 NewTable5_R3_Y1 <- NewTable4[NewTable4$Journal.Impact.Factor <= 3 & NewTable4$Publication.Year ==2021,]
 write.table(NewTable5_R3_Y1,file=paste0(PathName,"/",RVersion,"/",RVersion,"_Candidate_AuthorV3_TIFR3_Y1.txt"),
             row.names = F,col.names = TRUE, sep = '\t')
+write.table(NewTable5_R3_Y1,file=paste0(PathName,"/",RVersion,"/",RVersion,"_Candidate_AuthorV3_TIFR3_Y1.csv"),row.names = FALSE,sep = ',')
 
 
 
