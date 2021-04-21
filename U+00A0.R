b3ff0f2d0d8a11bc018fa09a000b88911a6a2b14
Ch_HCC <q2330416@gmail.com>
2021-04-21 14:20:31 +0800

    try ASCII

--- U+00A0.R
+++ U+00A0.R
@@ -9,9 +9,19 @@ FolderName = c("20210419")
 library(readxl)
 Author_list <- read_excel(paste0(PathName,"/author list_Su_3_Lee.xlsx"))
 
+Author_list2 <- as.matrix(Author_list)
+#Author_list3 <- as.character(Author_list2)
+Author_list3  <- gsub("\U00A0", "", Author_list2)
+
 # <U+00A0>
 # http://shihs-blog.logdown.com/posts/4773148-how-to-delete-u-00a0-in-r
 #NG# Author_list2 <- gsub("\U00A0", "",Author_list)
 
-write.table(Author_list, file=paste0(PathName,"/author list_Su_3_Lee.csv"),  
+#https://stackoverflow.com/questions/9934856/removing-non-ascii-characters-from-data-files
+
+# x %>% 
+#   tibble(name = .) %>%
+#   filter(xfun::is_ascii(name)== T)
+
+write.table(Author_list2, file=paste0(PathName,"/author list_Su_3_Lee2.csv"),  
             sep=",", row.names=FALSE)
